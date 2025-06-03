87bef62f0fed95c8a52e0bd803f9f7fea1ccedf0
ipurson <uropmabinipursonthomas@gmail.com>
2025-06-03 23:01:56 +0900

    Add .next/static/chunks/src_app_booking_page_tsx_8e739ecf._.js 1748959314140

---
+++ .next/static/chunks/src_app_booking_page_tsx_8e739ecf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_97ada26c._.js",
+  "static/chunks/src_a974f426._.js"
+],
+    source: "dynamic"
+});
